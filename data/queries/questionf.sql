d62e5bedc4626670df039b9908f6ffd9f8d66b36
Lucia Passeri <lucia.pas@hotmail.it>
2021-04-15 12:08:22 +0200

    FIX queries

--- data/queries/questionf.sql
+++ data/queries/questionf.sql
@@ -1,6 +1,7 @@
-CREATE STREAM questionf WITH (value_format='JSON') AS
-SELECT ref, name, value
+CREATE TABLE questionf WITH (value_format='JSON') AS
+SELECT ref, name, value, count(*) AS count
 FROM exploded_base
 WINDOW TUMBLING (SIZE 60 SECONDS)
 WHERE name='device_temperature' AND value > 35
+GROUP BY ref, name, value
 HAVING count(*) > 5;
